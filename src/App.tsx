a2f405850199cbd96bacb7753c4aa7cb6f7eb615
David Robson <david.robson@bookitude.com>
2020-02-08 12:11:19 +0000

    feat(firebase): add basic react-firebase integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {FirestoreCollection, FirestoreProvider} from "@react-firebase/firestore";
+import { config } from "./config/firebase";
+import firebase from "firebase";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FirestoreProvider {...config} firebase={firebase}>
+      <h1>Firebase test</h1>
+      <FirestoreCollection path="/users/" limit={1}>
+        {d => {
+          return d.isLoading ? "Loading" : <pre>{JSON.stringify(d.value)}</pre>;
+        }}
+      </FirestoreCollection>
+
+    </FirestoreProvider>
   );
-}
+};
 
 export default App;
